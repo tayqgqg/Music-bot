6986d44a0cfe728e899714224d00c9150a7b016d
tayqgqg <60991145+tayqgqg@users.noreply.github.com>
2025-04-16 13:38:20 +0700

    Update loader.js

--- loader.js
+++ loader.js
@@ -14,6 +14,20 @@ const playerEvents = readdirSync("./events/Player/").filter((file) =>
   file.endsWith(".js")
 );
 
+const fs = require('fs');
+const path = require('path');
+
+const commandFolders = fs.readdirSync(path.join(__dirname, 'commands'));
+
+for (const folder of commandFolders) {
+    const commandFiles = fs.readdirSync(path.join(__dirname, 'commands', folder)).filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+        const command = require(path.join(__dirname, 'commands', folder, file));
+        client.commands.set(command.name, command);
+    }
+}
+
 GetTranslationModule().then(() => {
   console.log("| Translation Module Loaded |");
 
