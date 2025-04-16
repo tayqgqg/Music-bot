7cfbe1ace1e2198feeb2fb50467d3ab2da7b616b
tayqgqg <60991145+tayqgqg@users.noreply.github.com>
2025-04-16 14:39:19 +0700

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,68 +1,66 @@
 const { readdirSync } = require("fs");
 const { Collection } = require("discord.js");
 const { useMainPlayer } = require("discord-player");
-const fs = require("fs");
-const path = require("path");
-const { Translate, GetTranslationModule } = require("./process_tools");
-
 client.commands = new Collection();
 const commandsArray = [];
 const player = useMainPlayer();
 
-const discordEvents = readdirSync(path.join(__dirname, "events", "Discord")).filter(file => file.endsWith(".js"));
-const playerEvents = readdirSync(path.join(__dirname, "events", "Player")).filter(file => file.endsWith(".js"));
+const { Translate, GetTranslationModule } = require("./process_tools");
 
-const commandFolders = fs.readdirSync(path.join(__dirname, 'commands'));
+const discordEvents = readdirSync("./events/Discord/").filter((file) =>
+  file.endsWith(".js")
+);
+const playerEvents = readdirSync("./events/Player/").filter((file) =>
+  file.endsWith(".js")
+);
 
 GetTranslationModule().then(() => {
   console.log("| Translation Module Loaded |");
 
-  // ** Load Discord Events **
-  discordEvents.forEach(file => {
-    const DiscordEvent = require(path.join(__dirname, "events", "Discord", file));
-    const eventName = file.split(".")[0];
-    const txtEvent = `< -> > [Loaded Discord Event] <${eventName}>`;
+  for (const file of discordEvents) {
+    const DiscordEvent = require(`./events/Discord/${file}`);
+    const txtEvent = `< -> > [Loaded Discord Event] <${file.split(".")[0]}>`;
     parseLog(txtEvent);
-    client.on(eventName, DiscordEvent.bind(null, client));
-    delete require.cache[require.resolve(path.join(__dirname, "events", "Discord", file))];
-  });
+    client.on(file.split(".")[0], DiscordEvent.bind(null, client));
+    delete require.cache[require.resolve(`./events/Discord/${file}`)];
+  }
 
-  // ** Load Player Events **
-  playerEvents.forEach(file => {
-    const PlayerEvent = require(path.join(__dirname, "events", "Player", file));
-    const eventName = file.split(".")[0];
-    const txtEvent = `< -> > [Loaded Player Event] <${eventName}>`;
+  for (const file of playerEvents) {
+    const PlayerEvent = require(`./events/Player/${file}`);
+    const txtEvent = `< -> > [Loaded Player Event] <${file.split(".")[0]}>`;
     parseLog(txtEvent);
-    player.events.on(eventName, PlayerEvent.bind(null));
-    delete require.cache[require.resolve(path.join(__dirname, "events", "Player", file))];
-  });
+    player.events.on(file.split(".")[0], PlayerEvent.bind(null));
+    delete require.cache[require.resolve(`./events/Player/${file}`)];
+  }
 
-  // ** Load Commands **
-  commandFolders.forEach(dirs => {
-    const commands = readdirSync(path.join(__dirname, 'commands', dirs)).filter(file => file.endsWith(".js"));
+  readdirSync("./commands/").forEach((dirs) => {
+    const commands = readdirSync(`./commands/${dirs}`).filter((files) =>
+      files.endsWith(".js")
+    );
 
-    commands.forEach(file => {
-      const command = require(path.join(__dirname, 'commands', dirs, file));
+    for (const file of commands) {
+      const command = require(`./commands/${dirs}/${file}`);
       if (command.name && command.description) {
         commandsArray.push(command);
         const txtEvent = `< -> > [Loaded Command] <${command.name.toLowerCase()}>`;
         parseLog(txtEvent);
         client.commands.set(command.name.toLowerCase(), command);
-        delete require.cache[require.resolve(path.join(__dirname, 'commands', dirs, file))];
+        delete require.cache[require.resolve(`./commands/${dirs}/${file}`)];
       } else {
-        const txtEvent = `< -> > [Failed Command] <${file}>`;
-        parseLog(txtEvent);
-      }
-    });
+        const txtEvent = `< -> > [Failed Command] <${command.name.toLowerCase()}>`;
+        parseLog(txtEvent);}
+    }
   });
 
-  // ** Register Slash Commands **
-  client.on("ready", () => {
-    const { global, guild } = client.config.app;
-    const commandSet = global ? client.application.commands.set(commandsArray) : client.guilds.cache.get(guild).commands.set(commandsArray);
+  client.on("ready", (client) => {
+    if (client.config.app.global)
+      client.application.commands.set(commandsArray);
+    else
+      client.guilds.cache
+        .get(client.config.app.guild)
+        .commands.set(commandsArray);
   });
 
-  // ** Log Translations **
   async function parseLog(txtEvent) {
     console.log(await Translate(txtEvent, null));
   }
